73b396f07a74e143bdd31068b9cab0a3facc64e6
Jaime Hernandez <jaimevazz@gmail.com>
2020-04-27 20:06:35 -0500

    Blog app up and running

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,16 @@ from django.db import models
 #Create a Blog
 class Blog(models.Model):    
     title = models.CharField(max_length=100)
-    summary = models.CharField(max_length=140)
+    # summary = models.CharField(max_length=140)
     image = models.ImageField(upload_to='images/blog/')
     pub_date = models.DateTimeField()
     content = models.TextField()
+
+    def summary(self):
+        return self.content[:33] + '...'
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e, %Y')
+
+    def __str__(self):
+        return self.title
