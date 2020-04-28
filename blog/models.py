55131910f4898ea8a073e68beb7af9a27c3a5211
Jaime Hernandez <jaimevazz@gmail.com>
2020-04-27 19:22:59 -0500

    Added Bootstrap4 homepage and jobs list

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+#Create a Blog
+class Blog(models.Model):    
+    title = models.CharField(max_length=100)
+    summary = models.CharField(max_length=140)
+    image = models.ImageField(upload_to='images/blog/')
+    pub_date = models.DateTimeField()
+    content = models.TextField()
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'Batimovile.90',
+        'PASSWORD': 'django1234',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
