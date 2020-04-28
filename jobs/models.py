019fbf4e902da111a47ddae0de4b1121b736a363
Jaime Hernandez <jaimevazz@gmail.com>
2020-04-27 20:24:14 -0500

    Jobs title attribute in admin

--- jobs/models.py
+++ jobs/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 class Job(models.Model):
+    title = models.CharField(max_length=140, default="Job title")
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
     tags = models.CharField(max_length=255, default="")
+
+    def __str__(self):
+        return self.title
