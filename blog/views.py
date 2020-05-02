df49846ad2197ea49546e4ea844637b7dee06815
Jaime Hernandez <jaimevazz@gmail.com>
2020-05-02 12:54:59 -0500

    Fixed Blog views

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,6 @@ def allblogs(request):
     blogs = Blog.objects
     return render(request, 'blog/allblogs.html', {'blogs':blogs})
 
-def detail(request, blog_id):
+def blog(request, blog_id):
     blog = get_object_or_404(Blog, pk=blog_id)
-    return render(request, 'blog/detail.html', {'blog':blog})
+    return render(request, 'blog/blog.html', {'blog':blog})
