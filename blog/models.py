aa24e87725e42880ab1d2b2272d97776c49d5cd4
Jaime Hernandez <jaimevazz@gmail.com>
2020-05-03 17:40:00 -0500

    Cleaned commets blog models

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,7 @@ from django.db import models
 
 #Create a Blog
 class Blog(models.Model):    
-    title = models.CharField(max_length=100)
-    # summary = models.CharField(max_length=140)
+    title = models.CharField(max_length=100)    
     image = models.ImageField(upload_to='images/blog/')
     pub_date = models.DateTimeField()
     content = models.TextField()
