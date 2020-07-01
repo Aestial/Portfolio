21971f0dc6369a8ced9bf1c448779deae8cc59ea
Jaime Hernandez <jaimevazz@gmail.com>
2020-07-01 15:27:48 -0500

    Updated  regletas with resize and dark colors

--- portfolio/static/js/numbers-grid.js
+++ portfolio/static/js/numbers-grid.js
@@ -1,104 +1,118 @@
-let type = "WebGL";
-
-if (!PIXI.utils.isWebGLSupported()){
-    type = "canvas";
-}
-PIXI.utils.sayHello(type);
-
-// Create a Pixi Application
-let app = new PIXI.Application({ 
-    width: 720,         // default: 800
-    height: 650,        // default: 600
-    antialias: true,    // default: false
-    transparent: false, // default: false
-    resolution: 1,       // default: 1
-    backgroundColor: 0xC9BAA0,
-  }
-);
-
-// Add the canvas that Pixi automatically created for you to the HTML document
-var domElements = document.getElementsByClassName('pixi-container');
-var domContainer = domElements[0];
-domContainer.appendChild(app.view);
-
-// To make responsive
-// app.renderer.autoResize = true;
-// app.renderer.resize(domContainer.innerWidth, domContainer.innerHeight);
-
 const GRID_SIZE = 12;
 const MAX_NUMBER = 10;
 
 const DOUBLE_TAP_THRESHOLD = 420;
 
 const UNIT_SIZE_PIXELS = 48;
-const UNIT_NORMAL_ALPHA = 0.66;
+const UNIT_NORMAL_ALPHA = 0.85;
 const UNIT_ACTIVE_ALPHA = 0.35;
 
-const UNIT_COLORS = [   0xFFFFFF, 
-                        0xD0423B, 
-                        0x8BA0C5,
-                        0xA88867,
-                        0x70C053,
-                        0x084A81,
-                        0xEFD647,
-                        0x93D1D0,
-                        0xEABEBD,
-                        0xEA7267,
-                    ];
+const UNIT_COLORS = [ 
+    0xFFFFFF, //#FFFFFF
+    0xD95863, //#D95863
+    0xCE5B7C, //#CE5B7C
+    0x6FB8D0, //#6FB8D0
+    0x5F6AC0, //#5F6AC0                        
+    0x70CD9D, //#70CD9D
+    0xB9D072, //#B9D072
+    0xE5C858, //#E5C858
+    0xEDC397, //#EDC397
+    0xE59179, //#E59179
+];
+
+let width = 672;
+let height = 672;
+let aspectRatio = width/height;
+
+// Create a Pixi Application
+let app = new PIXI.Application({ 
+    width: width,         // default: 672
+    height: height,        // default: 672
+    antialias: true,    // default: false
+    transparent: false, // default: false
+    resolution: 1,       // default: 1
+    backgroundColor: 0xE1E2E6
+});
 
 let lastTapTime, currentTapTime = 0;
+let canvas_container = document.getElementById("pixi-canvas-container");
 
-const grid = new PIXI.Container();
-app.stage.addChild(grid);
-
-// Create a grid of rectangles
-for (let i = 0; i < GRID_SIZE; i++) {
-    for (let j = 0; j < GRID_SIZE; j++) {
-        const cell = new PIXI.Graphics();
-        cell.lineStyle(1, 0x000000, 0.35);
-        cell.beginFill(0xc9cacb);
-        const x = (j * UNIT_SIZE_PIXELS) + 2 * UNIT_SIZE_PIXELS;
-        const y = (i * UNIT_SIZE_PIXELS) + UNIT_SIZE_PIXELS;
-        cell.drawRect(x, y, UNIT_SIZE_PIXELS, UNIT_SIZE_PIXELS);
-        cell.endFill();
-        cell.id = 1 + (i * j)
-        grid.addChild(cell);
+window.onload = function () 
+{    
+    let type = "WebGL";
+    if (!PIXI.utils.isWebGLSupported()){
+        type = "canvas";
     }
-}
+    PIXI.utils.sayHello(type);
+    
+    // Add the canvas that Pixi automatically created for you to the HTML document
+    canvas_container.appendChild(app.view);
+    app.view.className = "shadow";
 
-const container = new PIXI.Container();
-container.sortableChildren = true;
-app.stage.addChild(container);
+    const grid = new PIXI.Container();
+    app.stage.addChild(grid);
 
-// TEMP Create bars
-for (let i = 1; i <= MAX_NUMBER; i++) {
-    // createNumber(
-    //     Math.floor(Math.random() * app.screen.width),
-    //     Math.floor(Math.random() * app.screen.height),
-    //     i + 1,
-    // );
-    for (let j = 0; j < 1; j++)
-    {
-        let x = (i * j * UNIT_SIZE_PIXELS) + 5*UNIT_SIZE_PIXELS/2;
+    // Create a grid of rectangles
+    for (let i = 0; i < GRID_SIZE; i++) {
+        for (let j = 0; j < GRID_SIZE; j++) {
+            const cell = new PIXI.Graphics();
+            cell.lineStyle(1, 0x636C78, 0.5);
+            cell.beginFill(0x444E5E);
+            const x = (j * UNIT_SIZE_PIXELS) + UNIT_SIZE_PIXELS;
+            const y = (i * UNIT_SIZE_PIXELS) + UNIT_SIZE_PIXELS;
+            cell.drawRect(x, y, UNIT_SIZE_PIXELS, UNIT_SIZE_PIXELS);
+            cell.endFill();
+            cell.id = 1 + (i * j)
+            grid.addChild(cell);
+        }
+    }
+
+    const container = new PIXI.Container();
+    container.sortableChildren = true;
+    app.stage.addChild(container);    
+
+    // TEMP Create bars
+    let evenMultiplier = 0;
+    for (let i = 1; i <= MAX_NUMBER; i++) {
+        // createNumber(
+        //     Math.floor(Math.random() * app.screen.width),
+        //     Math.floor(Math.random() * app.screen.height),
+        //     i + 1,
+        // );
+        evenMultiplier += (i % 2 != 0) ? 1 : 0;
+        let x = (evenMultiplier) * UNIT_SIZE_PIXELS + (3*UNIT_SIZE_PIXELS/2);
         let y = i * UNIT_SIZE_PIXELS + UNIT_SIZE_PIXELS/2;
-        createNumber(x, y, i);
-    }    
+        createNumber(x, y, i, container);
+
+        // for (let j = 0; j < 1; j++)
+        // {
+        //     let x = (i * j * UNIT_SIZE_PIXELS) + 5*UNIT_SIZE_PIXELS/2;
+        //     let y = i * UNIT_SIZE_PIXELS + UNIT_SIZE_PIXELS/2;
+        //     createNumber(x, y, i);
+        // }    
+    }
+    window.addEventListener("resize", resize);
+    resize();
 }
 
-function createNumber(x, y, value) 
+// To make responsive
+// app.renderer.autoResize = true;
+// app.renderer.resize(domContainer.innerWidth, domContainer.innerHeight);
+
+function createNumber(x, y, value, parent)
 {
     let rectangle = new PIXI.Graphics();
-    rectangle.lineStyle(2, 0x000000, 0.85);
-    rectangle.beginFill(0xFFFFFF);
+    rectangle.lineStyle(3, 0x8A94A3); //#8A94A3
+    rectangle.beginFill(UNIT_COLORS[value-1]);
     rectangle.drawRect(0, 0, UNIT_SIZE_PIXELS * value, UNIT_SIZE_PIXELS);
     rectangle.endFill();
     
     var texture = app.renderer.generateTexture(rectangle);
 
     let sprite = new PIXI.Sprite(texture);
-    sprite.tint = UNIT_COLORS[value-1]; //Change with the color wanted
     sprite.alpha = UNIT_NORMAL_ALPHA;
-    
+    // sprite.tint = UNIT_COLORS[value-1]; //Change with the color wanted
+
     // enable the bunny to be interactive... this will allow it to respond to mouse and touch events
     sprite.interactive = true;
 
@@ -116,12 +130,15 @@ function createNumber(x, y, value)
     
     sprite.x = x;
     sprite.y = y;
-    sprite.zIndex = MAX_NUMBER - value;
+    // sprite.zIndex = MAX_NUMBER - value;
+    sprite.zIndex = value;
 
-    sprite.anchor.set(0.5/value, 0.5);
-
+    // sprite.anchor.set(0.5/value, 0.5);
+    let xanchor = (value % 2 == 0) ? -0.5/value : 0;
+    sprite.anchor.set(xanchor + 0.5, 0.5);
+ 
     // app.stage.addChild(rectangle);
-    container.addChild(sprite);
+    parent.addChild(sprite);
 }
 
 function onDragStart(event) {
@@ -156,5 +173,18 @@ function onTap() {
     lastTapTime = currentTapTime;
     currentTapTime = Date.now();
     if ((currentTapTime - lastTapTime) < DOUBLE_TAP_THRESHOLD)
-        this.rotation += Math.PI/2;    
+        this.rotation += Math.PI/4;    
 }
+
+function resize() {
+    let w = getParentDivWidth();
+    let h = w / aspectRatio ;    
+    app.stage.scale.set(w / width, h / height);
+    app.renderer.resize(w, h); 
+}
+
+function getParentDivWidth() {
+    let width = canvas_container.offsetWidth;
+    console.log("Width " + width);
+    return width;
+  }
