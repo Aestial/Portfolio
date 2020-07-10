b3fed1a4f2338220e283446c216ab4979060216d
Jaime Hernandez <jaimevazz@gmail.com>
2020-07-09 22:02:51 -0500

    About PDF: download as attachment

--- about/views.py
+++ about/views.py
@@ -29,8 +29,7 @@ def generate_pdf(request):
     eduexp = EducationExperience.objects.order_by('-start_date')
     certs = Certification.objects.all
     skills = Skill.objects.all
-    languages = Language.objects.all
-    
+    languages = Language.objects.all    
     # Rendered
     html_string = render_to_string('about/pdf.html', {
         'workexp': workexp,
@@ -42,19 +41,10 @@ def generate_pdf(request):
     html = HTML(string=html_string, base_url=request.build_absolute_uri())
     doc = html.render()
     pdf = doc.write_pdf()
-
     # Creating http response
     response = HttpResponse(pdf, content_type='application/pdf;')
     #Download as attachment
-    # response['Content-Disposition'] = 'attachment; filename=CV-{0}.pdf'.format("JHV-SoftwareDeveloper")
+    response['Content-Disposition'] = 'attachment; filename=CV-{0}.pdf'.format("JHV-SoftwareDeveloper")
     # Display in browser
-    response['Content-Disposition'] = 'inline; filename=list_people.pdf'
-    
-    # response['Content-Transfer-Encoding'] = 'binary'
-    # with tempfile.NamedTemporaryFile(delete=True) as output:
-    #     output.write(result)
-    #     output.flush()
-    #     output = open(output.name, 'r')
-    #     response.write(output.read())
-
+    # response['Content-Disposition'] = 'inline; filename=CV-{0}.pdf'.format("JHV-SoftwareDeveloper")
     return response
